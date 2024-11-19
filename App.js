a1cf3628a9585709030637e5bc47909cfc5dc446
Rafael Salmeron <rafasalmeronjr@gmail.com>
2024-11-18 22:23:18 -0300

    Criação

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {Lojas} from "./compents/loja/lojas";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      {
+        Lojas.map((loja) => (
+            <View key={loja.id}>
+                <Text>{loja.nome}</Text>
+                <Text>{loja.endereco}</Text>
+                <Text>{loja.telefone}</Text>
+                <Image source={loja.imagem} style={{ width: 150, height: 150 }} />
+            </View>
+            ))
+      }
+
     </View>
   );
 }
@@ -15,6 +28,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
