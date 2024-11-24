90ec8d530804e71960a1c95575b42b216bae154c
matheusgouvea <matheusgouvea@live.com>
2024-11-24 01:04:10 -0300

    Ajustes e adicao de metodo de pagamento, cupom e fechar pagamento

--- src/app/components/cardProduto/CardProduto.js
+++ src/app/components/cardProduto/CardProduto.js
@@ -1,22 +1,21 @@
 import React, { useContext } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { styles } from './style';
-import { CartContext } from '../../hooks/CartContext'; 
+import { CartContext } from '../../hooks/CartContext';
 
-const ProductCard = ({product}) => {
-    const { addItem } = useContext (CartContext);
+const ProductCard = ({ product }) => {
+    const { addItem } = useContext(CartContext);
 
     return (
         <View style={styles.cardContainer}>
             <View style={styles.productsContainer}>
                 <View key={product.id} style={styles.productItem}>
-                    <Image source={{uri: product.photo}} style={styles.productImage}/>
+                    <Image source={{ uri: product.photo }} style={styles.productImage} />
                     <Text style={styles.productName}>{product.name}</Text>
                     <Text style={styles.productPrice}>R$ {product.price.toFixed(2)}</Text>
                     <TouchableOpacity
                         style={styles.addButton}
-                        onPress={() => addItem(product)}
-                    >
+                        onPress={() => addItem(product)}>
                         <Text style={styles.addButtonText}>Adicionar ao Carrinho</Text>
                     </TouchableOpacity>
                 </View>
